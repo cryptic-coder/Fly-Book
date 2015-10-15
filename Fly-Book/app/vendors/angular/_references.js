7558236eaf3f620df0836c1749f4a2cfcf73a1f4
cryptic-coder <thomas.w.foreman@gmail.com>
2015-10-14 22:26:48 -0400

    Initial Setup

---
+++ Fly-Book/app/vendors/angular/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../app/vendors/jquery/jquery-2.1.4.js" />
+/// <reference path="../app/vendors/bootstrap/scripts/bootstrap.js" />
+/// <reference path="../app/vendors/angular/angular.js" />
+/// <reference path="../app/vendors/angular/angular-mocks.js" />
+/// <reference path="angular.js" />
